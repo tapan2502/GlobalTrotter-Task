af896d807d0c08664a9e311f8a96ad96d54c1ff1
tapan.upadhyay <tapan.upadhyay@floworx.io>
2025-03-01 14:30:53 +0530

    Updated backend API URL

--- globetrotter-frontend/src/api/authService.js
+++ globetrotter-frontend/src/api/authService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { setupInterceptors } from "./axiosConfig"
 
 // Create axios instance
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL || "https://globaltrotter-task.onrender.com/api"
 const api = axios.create({
   baseURL: API_URL,
   headers: {
